40f6dfb297d5ff717f8db95348888426ac273a26
Alexandre <alexandre.jankovic@epitech.eu>
2024-01-23 17:51:38 +0100

    MOD: Modified getPermanentTaxesAnnualizedTotal & getPermanentTaxesAnnualizedTotalAfterTaxes to make everything work with the freelance row + ADD: getAnnualSales, function re-used in both modified functions

--- src/helpers/calculations.ts
+++ src/helpers/calculations.ts
@@ -1,18 +1,31 @@
-import { PermanentTaxRate } from "../constants/taxRates";
+import { FreelanceTaxRate, PermanentTaxRate } from "../constants/taxRates";
 import { Taxes } from "../types/formTypes";
-import { sum } from 'lodash';
+import { multiply, sum } from 'lodash';
 
 export function getAnnualizedMonthlySalary(amount: number): number {
     return amount * 12;
 }
 
 export function getPermanentTaxesAnnualizedTotal(rows: Taxes["rows"]): number {
-    return sum(rows.map((row) => getAnnualizedMonthlySalary(row.monthlySalary)));
+    return sum(rows.map((row) => {
+        if (row.monthlySalary) {
+            return getAnnualizedMonthlySalary(row.monthlySalary);
+        } else {
+            return getAnnualSales(row.hourlyRate, row.dailyHours, row.daysByYear);
+        }
+    }));
+}
+
+export function getAnnualSales(hourlyRate:number, dailyHours:number, daysByYear:number): number {
+    return hourlyRate * dailyHours * daysByYear;
 }
 
 export function getPermanentTaxesAnnualizedTotalAfterTaxes(rows: Taxes["rows"]): number {
     return sum(rows.map((row) => {
-        const annualizedMonthlySalary = getAnnualizedMonthlySalary(row.monthlySalary);
-        return annualizedMonthlySalary * (1 - PermanentTaxRate);
+        if (row.monthlySalary) {
+            return getAnnualizedMonthlySalary(row.monthlySalary) * (1 - PermanentTaxRate);
+        } else {
+            return getAnnualSales(row.hourlyRate, row.dailyHours, row.daysByYear) * (1 - FreelanceTaxRate);
+        }
     }));
 }
