013f27ed8d9fabb7c5eebe740167e3a925d0b011
Alexandre <alexandre.jankovic@epitech.eu>
2024-01-23 17:55:33 +0100

    ADD: new fields for the validationSchema of the table

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -4,6 +4,9 @@ interface BaseTaxRow {
 
 export interface PermanentTaxesRow extends BaseTaxRow {
     monthlySalary: number;
+    hourlyRate: number;
+    dailyHours: number;
+    daysByYear: number;
 }
 
 export interface Taxes  {
