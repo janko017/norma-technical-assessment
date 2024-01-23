05e5232782fb02d8588a8111fa91b9d38d71fd77
Alexandre <alexandre.jankovic@epitech.eu>
2024-01-23 17:52:31 +0100

    ADD: freelance module added, everything works fine

--- src/components/Taxes.tsx
+++ src/components/Taxes.tsx
@@ -15,6 +15,9 @@ export const Taxes: React.FC = () => {
     const validationSchema: yup.ObjectSchema<FormTypes.Taxes> = yup.object({
         rows: yup.array().of(yup.object({
             monthlySalary: yup.number().required().min(0),
+            hourlyRate: yup.number().required().min(0), // Taux horaire
+            dailyHours: yup.number().required().min(0).max(24).integer(), // Heures par jour
+            daysByYear: yup.number().required().min(0).max(24).integer(), // Jours par années
             year: yup.number().required().min(1900).max(2100).integer()
         })).defined()
     })
@@ -31,8 +34,12 @@ export const Taxes: React.FC = () => {
         name: "rows"
     })
 
-    const onClickAppendRow = () => {
-        append({ monthlySalary: 0, year: 1900 })
+    const onClickAppendRow = (worker: "Employee" | "Freelance") => {
+        const rowToAdd = worker === "Employee"
+            ? { monthlySalary: 0, year: 1900 } as FormTypes.PermanentTaxesRow // Cast pour ne pas avoir de conflit avec l'inteface
+            : { hourlyRate: 0, dailyHours: 0, daysByYear: 0, year: 1900 } as FormTypes.PermanentTaxesRow;
+
+        append(rowToAdd);
     }
 
     const rows = useWatch({
@@ -68,6 +75,9 @@ export const Taxes: React.FC = () => {
                         <TableHead>
                             <TableRow>
                                 <TableCell>Salaire par mois</TableCell>
+                                <TableCell>Taux horaire</TableCell>
+                                <TableCell>Heures par jour</TableCell>
+                                <TableCell>Jours par an</TableCell>
                                 <TableCell>Année</TableCell>
                                 <TableCell />
                             </TableRow>
@@ -75,11 +85,33 @@ export const Taxes: React.FC = () => {
                         <TableBody>{
                             fields.map((field, index) =>
                                 <TableRow key={field.id}>
-                                    <TableCell>
-                                        <TextFieldElement control={control} name={`rows.${index}.monthlySalary`} InputProps={{
-                                            endAdornment: <InputAdornment position="end">€/ mois</InputAdornment>
-                                        }} />
-                                    </TableCell>
+
+                                    {/* Opération ternaires afin d'avoir les champs en fonction du revenu selectionné */}
+                                    
+                                    {field.monthlySalary !== undefined ? (
+                                        <TableCell>
+                                            <TextFieldElement control={control} name={`rows.${index}.monthlySalary`} InputProps={{
+                                                endAdornment: <InputAdornment position="end"> €/ mois</InputAdornment>
+                                            }} />
+                                        </TableCell>
+                                    ) : <TableCell/>}
+                                    {field.hourlyRate !== undefined ? (
+                                        <TableCell>
+                                            <TextFieldElement control={control} name={`rows.${index}.hourlyRate`} InputProps={{
+                                                endAdornment: <InputAdornment position="end"> €/ heure</InputAdornment>
+                                            }} />
+                                        </TableCell>
+                                    ) : <TableCell/>}
+                                    {field.dailyHours !== undefined ? (
+                                        <TableCell>
+                                            <TextFieldElement control={control} name={`rows.${index}.dailyHours`} InputProps={{ inputProps: { min: 1900 } }} />
+                                        </TableCell>
+                                    ) : <TableCell/>}
+                                    {field.daysByYear !== undefined ? (
+                                        <TableCell>
+                                            <TextFieldElement control={control} name={`rows.${index}.daysByYear`} InputProps={{ inputProps: { min: 1900 } }} />
+                                        </TableCell>
+                                    ) : <TableCell/>}
                                     <TableCell>
                                         <TextFieldElement control={control} name={`rows.${index}.year`} InputProps={{ inputProps: { min: 1900 } }} />
                                     </TableCell>
@@ -93,10 +125,12 @@ export const Taxes: React.FC = () => {
                     </Table>
                 </TableContainer>
                 <Stack direction="row" justifyContent="center" spacing={2} marginY={2}>
-                    <Button onClick={onClickAppendRow} sx={{ alignSelf: "center", marginTop: 2 }}>Ajouter un revenu de CDI</Button>
-                    <Button onClick={() => {
-                        //
-                    }} sx={{ alignSelf: "center", marginTop: 2 }}>Ajouter un revenu de Freelance</Button>
+
+                    {/* Opération onClick modifiée afin d'ajouter une nouvelle ligne en fonction d'un paramètre,
+                    celles qui sont destinées aux cdi et celles qui sont destinées aux freelance */}
+
+                    <Button onClick={() => onClickAppendRow("Employee")} sx={{ alignSelf: "center", marginTop: 2 }}>Ajouter un revenu de CDI</Button>
+                    <Button onClick={() => onClickAppendRow("Freelance")} sx={{ alignSelf: "center", marginTop: 2 }}>Ajouter un revenu de Freelance</Button>
                 </Stack>
                 <Typography variant="h6">Total : {total} €</Typography>
                 <Typography variant="h6">Total après taxes : {totalAfterTaxes} €</Typography>
